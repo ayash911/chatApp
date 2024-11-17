76767738e24b0712fa57acf4e06df76c8c2fbf57
ayash911 <ya828197@gmail.com>
2024-11-17 14:38:41 +0530

    vercel

--- index.js
+++ index.js
@@ -1,59 +1,12 @@
-const socketio = require("socket.io");
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const rateLimit = require("express-rate-limit");
-const crypto = require("crypto");
-const https = require("https");
-const fs = require("fs");
-
-const app = express();
-const server = https.createServer(
-  {
-    key: fs.readFileSync("server-key.pem"),
-    cert: fs.readFileSync("server-cert.pem"),
-  },
-  app
-);
-
-const io = socketio(server, {
+const io = require("socket.io")(8000, {
   cors: {
-    origin: ["https://127.0.0.1:5500"],
+    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
 const users = {};
-const JWT_SECRET_KEY = "your_secret_key";
-const AES_SECRET_KEY = "your_aes_secret_key";
-const AES_IV = crypto.randomBytes(16);
-
-// Encryption Function
-function encryptData(data) {
-  const cipher = crypto.createCipheriv("aes-256-cbc", AES_SECRET_KEY, AES_IV);
-  let encrypted = cipher.update(JSON.stringify(data), "utf-8", "hex");
-  encrypted += cipher.final("hex");
-  return encrypted;
-}
-
-function decryptData(encryptedData) {
-  const decipher = crypto.createDecipheriv(
-    "aes-256-cbc",
-    AES_SECRET_KEY,
-    AES_IV
-  );
-  let decrypted = decipher.update(encryptedData, "hex", "utf-8");
-  decrypted += decipher.final("utf-8");
-  return decrypted;
-}
-
-const rateLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 5,
-  message: "Too many requests, please try again later.",
-});
-
-app.use(rateLimiter);
 
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (name) => {
@@ -65,11 +18,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send", (message) => {
-    const encryptedMessage = encryptData(message);
-    socket.broadcast.emit("receive", {
-      message: encryptedMessage,
-      name: users[socket.id].name,
-    });
+    socket.broadcast.emit("receive", { message, name: users[socket.id].name });
   });
 
   socket.on("group-name-changed", (newGroupName) => {
@@ -97,8 +46,4 @@ io.on("connection", (socket) => {
       io.emit("update-user-list", users);
     }
   }
-});
-
-server.listen(8000, () => {
-  console.log("Server is running on https://127.0.0.1:8000");
 });
